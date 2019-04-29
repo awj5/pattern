f8c00ea7deb3bd4af8e5883301e078b3fd3d5880
Adam Johnson <adamjohnson@apple.com>
2019-04-29 14:40:37 +1000

    Initial

---
+++ pattern.js
@@ -0,0 +1,44 @@
+/* Pattern
+A vanilla JavaScript URL hash handler.
+https://github.com/adamwjohnson5/pattern
+By Adam Johnson
+MIT License 2019 */
+"use strict";
+
+class Pattern {
+    static hash() {
+        window.section; // Global var
+        // Listen for URL hash change
+        window.addEventListener('hashchange', hashHandler);
+        hashHandler();
+    }
+}
+
+/* URL hash */
+
+function hashHandler() {
+    let i = location.hash; // Get URL hash
+    // Get section
+    var urlSection = i.replace('#', '');
+    urlSection = urlSection.replace(/\//g, '');
+    if (i.indexOf('?') !== -1) {
+        urlSection = urlSection.substr(0, urlSection.indexOf('?'));
+    }
+    // Set section to 'home' if not defined
+    if (!urlSection) {
+        urlSection = 'home';
+    }
+    // Call section function if exists
+    const func = window[urlSection + 'Load'];
+    if (urlSection !== window.section && typeof func === 'function') {
+        func();
+    }
+    window.section = urlSection; // Set section global var
+    // Get URL params
+    var urlParams = i.split('?').pop();
+    urlParams = new URLSearchParams(urlParams);
+    // Loop all params and create global vars
+    for (let x of urlParams.entries()) {
+        window[x[0]] = x[1];
+    }
+}
