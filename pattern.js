796fec1106a0c37501ab0cde567c41ff77d2ae0c
Adam Johnson <adamjohnson@apple.com>
2020-06-09 16:18:37 -0700

    Update pattern.js

--- pattern.js
+++ pattern.js
@@ -13,12 +13,13 @@ Pattern.init();
 history.pushState(null, null, 'about'); // Change URL with pushState
 
 */
+
 "use strict";
 
 class Pattern {
-    
     static init() {
         window.section; // Global var
+        
         // Listen for URL change
         window.addEventListener('popstate', urlHandler); // Listen for browser back and forward buttons
         const pushState = history.pushState;
@@ -28,28 +29,32 @@ class Pattern {
         };
         urlHandler(); // Fire on init
     }
-    
 }
 
 function urlHandler() {
     // Get section
     var urlSection = location.pathname.replace('/', '');
+    
     // Check for params
     if (urlSection.indexOf('?') !== -1) {
         urlSection = urlSection.substr(0, urlSection.indexOf('?')); // Remove params from section
     }
+    
     // Rename to 'home' if section empty
     if (! urlSection) {
         urlSection = 'home';
     }
     window.section = urlSection; // Set section global var
+    
     // Get URL params
     var urlParams = location.search.split('?').pop();
     urlParams = new URLSearchParams(urlParams);
+    
     // Loop all params and create global vars
     for (let x of urlParams.entries()) {
         window[x[0]] = x[1]; // Set global var
     }
+    
     // Call section function if exists
     const func = window[urlSection + 'Load'];
     if (typeof func === 'function') {
